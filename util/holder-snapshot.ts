8a00fad0979787981b77e446df59009f20171c0c
Alice <alize_in_cryptoland@protonmail.com>
2022-05-18 18:14:45 +0200

    re-add holder snapshot

--- util/holder-snapshot.ts
+++ util/holder-snapshot.ts
@@ -1,67 +1,33 @@
-import { from } from "rxjs";
-import { mergeMap, toArray, map, tap } from "rxjs/operators";
-import { TOKEN_PROGRAM_ID } from "./accounts";
-let holders = {};
-let i = 0;
-const getTokenHolder = (url, key, setCounter, token) => {
+import { Connection, ParsedAccountData, PublicKey } from "@solana/web3.js";
+import { from, lastValueFrom } from "rxjs";
+import { mergeMap, retry } from "rxjs/operators";
 
-  return fetch(url, {
-    body: `{
-        "jsonrpc":"2.0", 
-        "id":1, 
-        "method":"getProgramAccounts", 
-        "params":[
-          "${TOKEN_PROGRAM_ID}",
-          {
-            "encoding": "jsonParsed",
-            "filters": [
-              {
-                "dataSize": 165
-              },
-              {
-                "memcmp": {
-                  "offset": 0,
-                  "bytes": "${key}"
-                }
-              }
-            ]
-          }
-        ]}
-    `,
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + token,
-    },
-    method: "POST",
-  })
-    .then((res) => res.json())
-    .then(async (res) => {
-      i++;
-      setCounter(i + 1);
-      res.result.forEach((r, i) => {
-        if (r.account.data.parsed.info.tokenAmount.uiAmount > 0) {
-          if (!holders[r.account.data.parsed.info.owner]) {
-            holders[r.account.data.parsed.info.owner] = {
-              amount: +r.account.data.parsed.info.tokenAmount.uiAmount,
-              mints: [key],
-            };
-          } else {
-            holders[r.account.data.parsed.info.owner].amount += +r.account.data.parsed.info.tokenAmount.uiAmount;
-            holders[r.account.data.parsed.info.owner].mints.push(key);
-          }
-        }
-      });
-    });
-};
-
-export const getHolders = (mintIds: string[], setCounter, url, token) => {
-  return from(mintIds).pipe(
-    mergeMap((id) => getTokenHolder(url, id, setCounter, token), 10),
-    toArray(),
-    map(() => ({ ...holders })),
-    tap(() => {
-      holders = {};
-      i = 0;
-    })
+export async function getOwners(
+  mints: string[],
+  connection: Connection,
+  setCounter
+) {
+  let all_owners = [];
+  const mints_obs = from(mints).pipe(
+    mergeMap(async (mint) => {
+      const token_account = (
+        await connection.getTokenLargestAccounts(new PublicKey(mint))
+      ).value[0].address;
+      const token_account_info = await connection.getParsedAccountInfo(
+        token_account
+      );
+      return (token_account_info?.value?.data as ParsedAccountData)?.parsed?.info?.owner;
+    }, 10),
+    retry(5)
   );
-};
+  mints_obs.subscribe((e) => {
+    if (!(e in all_owners)) {
+      all_owners.push(e);
+      setCounter(all_owners.length);
+    }
+  });
+  if (await lastValueFrom(mints_obs)) {
+    console.log(all_owners.length);
+    return all_owners;
+  }
+}

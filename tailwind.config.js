cc8e7bf57991dcd2e815d00a9e3b8d978be523f6
Alice <alize_in_cryptoland@protonmail.com>
2022-05-23 17:23:46 +0200

    add context to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   purge: [
     "./pages/**/*.{js,ts,jsx,tsx}",
+    "./contexts/**/*.{js,ts,jsx,tsx}",
     "./providers/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],

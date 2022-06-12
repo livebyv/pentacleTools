fe3fd128492b008902cb978d8e01b79ae152eaf2
Alice <alize_in_cryptoland@protonmail.com>
2022-06-12 22:31:29 +0200

    add stuffs

---
+++ contexts/BalanceProvider.tsx
@@ -0,0 +1,83 @@
+import { getAssociatedTokenAddress } from "@solana/spl-token";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { createContext, useContext, useEffect, useReducer } from "react";
+import { SHDW_TOKEN } from "../util/accounts";
+import { StringPublicKey } from "../util/token-metadata";
+
+const initState = {
+  balance: {
+    sol: 0,
+    shdw: 0,
+  },
+  loading: false,
+};
+
+const BalanceContext = createContext(initState);
+
+export function BalanceProvider({ children }: { children: JSX.Element }) {
+  const [state, dispatch] = useReducer(
+    (
+      state: typeof initState,
+      action:
+        | { type: "loading"; payload?: { loading: boolean } }
+        | { type: "solBalance"; payload?: { solBalance: StringPublicKey } }
+        | { type: "shdwBalance"; payload?: { shdwBalance: StringPublicKey } }
+    ) => {
+      switch (action.type) {
+        case "loading":
+          return { ...state, loading: action.payload.loading };
+        case "solBalance":
+          return { ...state, solBalance: action.payload.solBalance };
+        case "shdwBalance":
+          return { ...state, shdwBalance: action.payload.shdwBalance };
+        default:
+          throw new Error("unsupported action type given on SendNFTs reducer");
+      }
+    },
+    initState
+  );
+
+  const { publicKey } = useWallet();
+  const { connection } = useConnection();
+
+  useEffect(() => {
+    const iv = setInterval(async () => {
+      dispatch({
+        type: "solBalance",
+        payload: {
+          solBalance: (
+            (await connection.getBalance(publicKey)) / LAMPORTS_PER_SOL
+          ).toFixed(6),
+        },
+      });
+      dispatch({
+        type: "shdwBalance",
+        payload: {
+          shdwBalance: (
+            await connection.getTokenAccountBalance(
+              await getAssociatedTokenAddress(SHDW_TOKEN, publicKey)
+            )
+          ).value.uiAmount.toFixed(2),
+        },
+      });
+    }, 10000);
+    return () => clearInterval(iv);
+  }, []);
+
+  return (
+    <BalanceContext.Provider
+      value={{
+        balance: {
+          shdw: state.balance.shdw,
+          sol: state.balance.sol,
+        },
+        loading: state.loading,
+      }}
+    >
+      {children}
+    </BalanceContext.Provider>
+  );
+}
+
+export const useBalance = () => useContext(BalanceContext);
---
+++ util/validator-batcher.ts
@@ -0,0 +1,119 @@
+import { VoteAccountInfo, ValidatorInfo, PublicKey } from "@solana/web3.js";
+import { sleep } from "./sleep";
+import { ValidatorApy } from "./stakeviewApp";
+import { ValidatorScore } from "./validatorsApp";
+export interface ValidatorMeta {
+  voteAccountInfo: VoteAccountInfo;
+  validatorInfo: ValidatorInfo | undefined;
+  validatorScore: ValidatorScore | undefined;
+  validatorApy: ValidatorApy | undefined;
+}
+const BATCH_SIZE = 100;
+
+export async function validatorBatcher(
+  voteAccountStatus: VoteAccountInfo[],
+  validatorInfos: ValidatorInfo[],
+  validatorScores: ValidatorScore[],
+  validatorApys: ValidatorApy[],
+  onValidatorMetas: (metas: ValidatorMeta[]) => void,
+  abortSignal: AbortSignal
+) {
+  let validatorMetas: ValidatorMeta[] = [];
+  let remainingVoteAccountInfos = [...voteAccountStatus];
+  let remainingValidatorInfos = [...validatorInfos];
+  let remainingValidatorApys = [...validatorApys];
+
+  console.log("scores", validatorScores.length);
+
+  for (let i = 0; i < validatorScores.length; i++) {
+    const validatorScore = validatorScores[i];
+    const voteAccountIndex = remainingVoteAccountInfos.findIndex(
+      (info) => info.nodePubkey === validatorScore.account
+    );
+    if (voteAccountIndex < 0) {
+      // If score does not match anything then it goes into the no score bucket
+      continue;
+    }
+    const [voteAccountInfo] = remainingVoteAccountInfos.splice(
+      voteAccountIndex,
+      1
+    );
+
+    const validatorInfoIndex = remainingValidatorInfos.findIndex(
+      (validatorInfo) =>
+        validatorInfo.key.equals(new PublicKey(voteAccountInfo.nodePubkey))
+    );
+    let validatorInfo: ValidatorInfo | undefined;
+    [validatorInfo] =
+      validatorInfoIndex > -1
+        ? remainingValidatorInfos.splice(validatorInfoIndex, 1)
+        : [];
+
+    const validatorApyIndex = remainingValidatorApys.findIndex(
+      (validatorApy) => validatorApy.id === voteAccountInfo.nodePubkey
+    );
+    let validatorApy: ValidatorApy | undefined;
+    [validatorApy] =
+      validatorApyIndex > -1
+        ? remainingValidatorApys.splice(validatorApyIndex, 1)
+        : [];
+
+    validatorMetas.push({
+      voteAccountInfo,
+      validatorInfo,
+      validatorScore,
+      validatorApy,
+    });
+
+    if (i % BATCH_SIZE === 0) {
+      await sleep(1);
+      console.log(`batch index: ${i}`);
+      onValidatorMetas([...validatorMetas]);
+    }
+
+    if (abortSignal.aborted) {
+      return;
+    }
+  }
+
+  for (let i = 0; i < remainingVoteAccountInfos.length; i++) {
+    const voteAccountInfo = remainingVoteAccountInfos[i];
+
+    const validatorInfoIndex = remainingValidatorInfos.findIndex(
+      (validatorInfo) =>
+        validatorInfo.key.equals(new PublicKey(voteAccountInfo.nodePubkey))
+    );
+    let validatorInfo: ValidatorInfo | undefined;
+    [validatorInfo] =
+      validatorInfoIndex > -1
+        ? remainingValidatorInfos.splice(validatorInfoIndex, 1)
+        : [];
+
+    const validatorApyIndex = remainingValidatorApys.findIndex(
+      (validatorApy) => validatorApy.id === voteAccountInfo.nodePubkey
+    );
+    let validatorApy: ValidatorApy | undefined;
+    [validatorApy] =
+      validatorApyIndex > -1
+        ? remainingValidatorApys.splice(validatorApyIndex, 1)
+        : [];
+
+    validatorMetas.push({
+      voteAccountInfo,
+      validatorInfo,
+      validatorScore: undefined,
+      validatorApy,
+    });
+
+    if (i % BATCH_SIZE === 0) {
+      await sleep(1);
+      console.log(`batch index: ${i}`);
+      onValidatorMetas([...validatorMetas]);
+    }
+
+    if (abortSignal.aborted) {
+      return;
+    }
+  }
+  return validatorMetas;
+}
